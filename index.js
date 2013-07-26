95cfd14bf7be61db26642dae1559e40fb31d0969
Adrian Rossouw <adrian@daemon.co.za>
2013-07-26 23:03:50 +0200

    added bootstrap assets

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var Graft = require('graftjs/server');
+var express = require('express');
+var Graft   = require('graftjs/server');
+var debug   = require('debug')('graft:bootstrap');
 
-Graft.addInitializer(function(opts) {
-
-});
+Graft.on('after:mount:static', function(opts) {
+    debug('mounting bootstrap assets');
+    this.use('/assets', express.static(__dirname + '/assets'));
+}, Graft);
